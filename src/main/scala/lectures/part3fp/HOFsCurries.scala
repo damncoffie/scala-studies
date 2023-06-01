c655e6455c6bbf2569347139e9a2227ee507a8ea
Mikhail_Bobriashov <bobryash@gmail.com>
2023-06-01 13:32:32 +0200

    Minor: + HOF comments and formatting + gitignore update

--- src/main/scala/lectures/part3fp/HOFsCurries.scala
+++ src/main/scala/lectures/part3fp/HOFsCurries.scala
@@ -8,7 +8,7 @@ package lectures.part3fp
 * */
 object HOFsCurries extends App {
 
-  val superFunction: (Int,(String, (Int => Boolean)) => Int) => (Int => Int) = null
+  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
   // higher order function (HOF)
 
   // map, flatMap, filter in MyList
@@ -19,33 +19,37 @@ object HOFsCurries extends App {
   // nTimes(f, n, x) = f(f(...f(x))) = nTimes(f, n-1, f(x))
   def nTimes(f: Int => Int, n: Int, x: Int): Int =
     if (n <= 0) x
-    else nTimes(f, n-1, f(x))
+    else nTimes(f, n - 1, f(x))
 
   val plusOne = (x: Int) => x + 1
   println(nTimes(plusOne, 10, 1))
+
 
   // ntb(f, n) = x => f(f(f...x)))
   // increment10 = ntb(plusOne, 10) = x => plusOne(plusOne....(x))
   // val y = increment10(1)
   def nTimesBetter(f: Int => Int, n: Int): (Int => Int) =
-    if (n <= 0) (x: Int) => x
+    if (n <= 0) (x: Int) => x // identity function - function that returns its input unchanged
     else (x: Int) => nTimesBetter(f, n - 1)(f(x))
 
   val plus10 = nTimesBetter(plusOne, 10)
   println(plus10(1))
 
+
   // curried functions
+  // Currying is a technique in functional programming where a function that takes multiple arguments is transformed
+  // into a sequence of functions, each taking a single argument.
   val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
   val add3 = superAdder(3) // y => 3 + y
   println(add3(10))
   println(superAdder(3)(10))
 
+
   // functions with multiple parameter lists
   def curriedFormatter(c: String)(x: Double): String = c.format(x)
 
-  val standardFormat: (Double => String) = curriedFormatter(("%4.2f"))
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
   val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
-
   println(standardFormat(Math.PI))
   println(preciseFormat(Math.PI))
 }
